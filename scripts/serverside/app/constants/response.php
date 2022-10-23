e9c18a3e64ce454ec2eac72c73144d88370d5135
Kyle <kyle.treoxer@gmail.com>
2022-10-23 15:08:44 +0700

    feat: logout in auth

--- scripts/serverside/app/constants/response.php
+++ scripts/serverside/app/constants/response.php
@@ -4,3 +4,7 @@
 define('API_NOT_FOUND', 'api_not_found');
 define('METHOD_NOT_ALLOWED', 'method_not_allowed');
 define('WRONG_API_CALL', 'wrong_api_call');
+
+#==== LOGIN API RESPONSE
+define('ALREADY_LOGIN', 'already_login');
+define('ACCOUNT_NOT_FOUND', 'account_not_found');
