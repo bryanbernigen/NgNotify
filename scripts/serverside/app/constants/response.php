c9fdf1486c5844101f160342d8c91af6a6bf62ba
Kyle <kyle.treoxer@gmail.com>
2022-10-23 16:26:19 +0700

    feat: Register Endpoint

--- scripts/serverside/app/constants/response.php
+++ scripts/serverside/app/constants/response.php
@@ -8,3 +8,7 @@ define('WRONG_API_CALL', 'wrong_api_call');
 #==== LOGIN API RESPONSE
 define('ALREADY_LOGIN', 'already_login');
 define('ACCOUNT_NOT_FOUND', 'account_not_found');
+
+#==== REGISTER API RESPONSE
+define('EMAIL_REGISTERED', 'email_registered');
+define('USERNAME_REGISTERED','username_registered');
