be4f2d10918001defa74b392d72ee55e67be1bf7
Bryan Bernigen <bryanbernigen@gmail.com>
2022-10-23 14:40:24 +0700

    added User model for user database access

--- scripts/serverside/app/models/User.php
+++ scripts/serverside/app/models/User.php
@@ -1,23 +1,54 @@
 <?php
 
-class User {
-    private $table = 'sql6527403.user';
+class User
+{
+    private $table = 'users';
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         require_once __DIR__ . '/../core/Database.php';
         $this->db = new Database;
     }
 
-    public function getLogin($username, $password) {
-        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE username = :username AND password = :password');
-        $this->db->bind(':username', $username);
-        $this->db->bind(':password', $password);
+    public function login($email, $password)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE email = :email');
+        $this->db->bind(':email', $email);
+        $this->db->execute();
         $row = $this->db->single();
-        if ($row) {
+        if (password_verify($password, $row['password'])) {
             return $row;
         } else {
             return false;
         }
+    }
+
+    public function register($email, $password, $username)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE email = :email');
+        $this->db->bind(':email', $email);
+        $this->db->execute();
+        $row = $this->db->single();
+        if ($row) {
+            return false;
+        } else {
+            $this->db->query('INSERT INTO ' . $this->table . ' VALUES (default, :email, :password, :username, true)');
+            $this->db->bind(':email', $email);
+            $this->db->bind(':username', $username);
+            $this->db->bind(':password', password_hash($password, PASSWORD_DEFAULT));
+            $this->db->execute();
+            return true;
+        }
+    }
+
+    public function changePassword($email, $password)
+    {
+        $this->db->query('UPDATE ' . $this->table . ' SET password = :password WHERE email = :email');
+        $this->db->bind(':email', $email);
+        $password = crypt($password, 'tubes1wbd');
+        $this->db->bind(':password', $password);
+        $this->db->execute();
+        return true;
     }
 }
