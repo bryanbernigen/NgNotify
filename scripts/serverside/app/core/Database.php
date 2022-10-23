32d68d09ad2fe24281fc19acf9e6f1c26c0138b1
Bryan Bernigen <bryanbernigen@gmail.com>
2022-10-23 18:36:48 +0700

    Change database connection timeout to 10munites

--- scripts/serverside/app/core/Database.php
+++ scripts/serverside/app/core/Database.php
@@ -9,7 +9,8 @@ class Database{
     {
         $option = [
             PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_TIMEOUT => 2
         ];
 
         try {
