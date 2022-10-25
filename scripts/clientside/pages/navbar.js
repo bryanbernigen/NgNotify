b35d2cea8294007f51bcf0fa018eaf8b38e95a38
Bryan Bernigen <bryanbernigen@gmail.com>
2022-10-25 09:45:46 +0700

    Added Navbar js logic

---
+++ scripts/clientside/pages/navbar.js
@@ -0,0 +1,33 @@
+function infoNavbar(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            let res = JSON.parse(this.responseText);
+            username = document.getElementById("uname");
+            if(res['status']){
+                echos = res['data'];
+                username.innerHTML = res['data']['username'];
+            }
+            else {
+                username.innerHTML = "Guest";
+                document.getElementById("loginout").innerHTML = "Login";
+            }
+        }
+    };
+    xhttp.open("GET","http://localhost:8000/api/auth/info",true);
+    xhttp.setRequestHeader("Accept", "application/json");
+    xhttp.withCredentials = true;
+    xhttp.send();
+
+    if (username.innerHTML == "Guest"){
+        document.getElementById("loginout").innerHTML = "Login";
+    }
+}
+
+function redirectToAlbum(){
+    window.location.href = "http://localhost:8080/pages/albums/albums.html";
+}
+
+function redirectToHome(){
+    window.location.href = "http://localhost:8080/pages/home/home.html";
+}
