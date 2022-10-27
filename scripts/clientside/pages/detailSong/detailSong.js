ba96909ce9877e8c6c3fea9a03df3c94a28df2b0
Maharani Ayu Putri Irawan <72875641+rannnayy@users.noreply.github.com>
2022-10-27 07:24:04 +0700

    finished searchSortFilter and detailSong

---
+++ scripts/clientside/pages/detailSong/detailSong.js
@@ -0,0 +1,183 @@
+songDetail = [
+    {
+        "title": "Nxde",
+        "singer": "(G)-IDLE",
+        "duration": "2:58",
+        "audio_path": "http://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.mp4",
+        "image_path": "../../assets/sample-song-image.jpg",
+        "release_date": "2022-10-17",
+        "lyrics": ["Why you think that 'bout nude? 'Cause your view's so rude","Think outside the box, then you'll like it","Hello, my name is 예삐, 예삐요 (hello)","말투는 멍청한 듯 몸매는 sexy, sexy요"," \
+                    그럼, 다이아 박힌 티아라 하나에","내가 퍽이나 웃게 (S.Y.), 퍽이나 웃게 (ooh)","뒤틀려버린 로렐라이, don't need no man (yah)","철학에 미친 독서광 (huh), self-made woman","싸가지없는 이 story에 무지 황당한"," \
+                    야유하는 관객들, you tricked me, you're a liar","아, 발가벗겨져 버린 movie star (movie star)","아, 별빛이 깨져버린 밤","꼴이 볼품없대도, 망가진다 해도 (uh)","다신 사랑받지 못한대도","쉿, yes, I'm a nude"," \
+                    Nude (따따랏따라)","Yes, I'm a nude","Nude (I don't give a love)","Baby, how do I look? (야)","How do I look? (야)","아리따운 날 입고, 따따랏따라", "Baby, how do I look? (야)", "How do I look? (야)"],
+        "album": "I Love",
+    },
+]
+
+audio = document.getElementById('addAudio');
+playmusicBt1 = document.getElementById("play");
+playmusicBt2 = document.getElementById("playmusic");
+timelineAudio = document.getElementById("timelineAudio");
+timelinePassed = document.getElementById("passedTime");
+timelineRemain = document.getElementById("remainingTime");
+soundButton = document.getElementById("vol");
+soundVolume = document.getElementById("timelineVol");
+repeatButton = document.getElementById("repeat");
+
+function appendData() {
+    var div1 = document.getElementById("albumPoster");
+    div1.src = songDetail[0].image_path;
+    div1.style.width = "15vw";
+
+    var div2 = document.getElementById("albumTitle");
+    div2.innerHTML += songDetail[0].title;
+    div2.style.width = "50vw";
+
+    var div3 = document.getElementById("albumSinger");
+    div3.innerHTML += songDetail[0].singer;
+    div3.style.fontFamily = "CircularStd-Bold";
+    div3.style.color = "#FFFFFF";
+    div3.style.width = "max-content";
+
+    var div4 = document.getElementById("albumYear");
+    div4.innerHTML += songDetail[0].release_date;
+    div4.style.fontFamily = "CircularStd-Medium";
+    div4.style.color = "#FFFFFF";
+    div4.style.width = "max-content";
+
+    var div6 = document.getElementById("albumDuration");
+    div6.innerHTML += songDetail[0].duration;
+    div6.style.fontFamily = "CircularStd-Light";
+    div6.style.color = "#6C6C6C";
+    div6.style.width = "max-content";
+    
+    var div7 = document.getElementById("songLyrics");
+    for (let i = 0; i < songDetail[0].lyrics.length; i++) {
+        div7.innerHTML += songDetail[0].lyrics[i] + "<br>";
+    }
+    div7.style.fontFamily = "CircularStd-Book";
+    div7.style.fontSize = "16px";
+    div7.style.color = "#B3B3B3";
+    div7.style.marginBottom = "0.5vh";
+}
+
+function playMusic(num) {
+    if (audio.paused == false) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    
+    var div8 = document.getElementById("addAudio");
+    var div9 = document.getElementById("sourceAudio");
+    div9.src = songDetail[num]["audio_path"];
+    div8.load();
+
+    timelinePassed.innerHTML = "0:00";
+    timelinePassed.style.color = "white";
+    timelinePassed.style.fontFamily = "CircularStd-Light";
+    timelineRemain.innerHTML = "0:00";
+    timelineRemain.style.color = "white";
+    timelinePassed.style.fontFamily = "CircularStd-Light";
+
+    audio.volume = 0.2;
+    soundVolume.background = 'linear-gradient(to right, #1ED760 0%, #1ED760 ' + 20 + '%, #6C6C6C ' + 20 + '%, #6C6C6C 100%)';
+    soundVolume.value = 20;
+
+    var div10 = document.getElementsByClassName("musicPlayerPoster");
+    for (let i=0; i < div10.length; i++) {
+        div10[i].src = songDetail[num].image_path;
+        div10[i].style.width = "5vw";
+    }
+
+    var div11 = document.getElementById("musicPlayerTitle");
+    div11.innerHTML += songDetail[num]["title"];
+    div11.style.fontFamily = "CircularStd-Bold";
+    div11.style.fontSize = "14px";
+    div11.style.color = "#FFFFFF";
+    div11.style.width = "max-content";
+
+    var div12 = document.getElementsByClassName("musicPlayerSinger");
+    for (let i=0; i < div12.length; i++) {
+        div12[i].innerHTML += songDetail[num]["singer"];
+        div12[i].style.color = "#FFFFFF";
+        div12[i].style.width = "max-content";
+    }
+    
+    var div13 = document.getElementById("musicAlbum");
+    div13.innerHTML += songDetail[num]["album"];
+    div13.style.color = "#FFFFFF";
+    div13.style.width = "max-content";
+}
+
+function toggleAudioPlay () {
+    if (audio.paused || audio.played == undefined) {
+        audio.play();
+        playmusicBt1.className = "fas fa-pause play";
+        playmusicBt2.className = "fas fa-pause playmusic";
+    }
+    else {
+        audio.pause();
+        playmusicBt1.className = "fas fa-play play";
+        playmusicBt2.className = "fas fa-play playmusic";
+    }
+}
+
+function changeTimelineAudioPosition () {
+    const percentage = (100*audio.currentTime) / audio.duration;
+    timelineAudio.style.background = 'linear-gradient(to right, #1ED760 0%, #1ED760 ' + percentage + '%, #6C6C6C ' + percentage + '%, #6C6C6C 100%)';
+    timelineAudio.value = percentage;
+    timelinePassed.innerHTML = Math.floor(Math.floor(audio.currentTime)/60) + ":" + (Math.floor(audio.currentTime)%60);
+    timelinePassed.style.color = "white";
+    timelinePassed.style.fontFamily = "CircularStd-Light";
+    timelineRemain.innerHTML = Math.floor(Math.floor(audio.duration-audio.currentTime)/60) + ":" + (Math.floor(audio.duration-audio.currentTime)%60);
+    timelinePassed.style.color = "white";
+    timelinePassed.style.fontFamily = "CircularStd-Light";
+}
+
+function audioEnded () {
+    audio.currentTime = 0;
+    playmusicBt1.className = "fas fa-play play";
+    playmusicBt2.className = "fas fa-play playmusic";
+}
+
+function changeSeekTimeline () {
+    const time = (timelineAudio.value * audio.duration) / 100;
+    audio.currentTime = time;
+}
+
+function toggleSound () {
+    audio.muted = !audio.muted;
+    soundButton.className = audio.muted ? "fas fa-volume-mute volume" : "fas fa-volume-up volume";
+}
+
+function rangeSoundVol () {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #1ED760 0%, #1ED760 ' + value + '%, #6C6C6C ' + value + '%, #6C6C6C 100%)';
+    audio.volume = value/100;
+    if (value == 0) {
+        soundButton.className = "fas fa-volume-mute volume";
+    }
+    else {
+        soundButton.className = "fas fa-volume-up volume";
+    };
+}
+
+function toggleRepeat () {
+    audio.loop = !audio.loop;
+    repeatButton.className = audio.loop ? "fas fa-ban repeat" : "fas fa-redo-alt repeat";
+}
+
+audio.ontimeupdate = function() {changeTimelineAudioPosition()};
+audio.onended = audioEnded;
+playmusicBt1.addEventListener('click', toggleAudioPlay);
+playmusicBt2.addEventListener('click', toggleAudioPlay);
+timelineAudio.addEventListener('change', changeSeekTimeline);
+soundButton.addEventListener('click', toggleSound);
+soundVolume.addEventListener('change', rangeSoundVol);
+repeatButton.addEventListener('click', toggleRepeat);
+
+window.onload = function() {
+    appendData();
+    playMusic(0);
+    console.log("udah masuk");
+}
---
+++ scripts/clientside/pages/searchSortFilter/searchSortFilter.js
@@ -0,0 +1,305 @@
+songList = [
+    {
+        "Judul": "Anti fragile",
+        "Penyanyi": "Le Sserafim",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+        "Duration": "232",
+        "Audio_path": "https://www.youtube.com/watch?v=pyf8cbqyfPs",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "album_id": "1",
+    },
+    {
+        "Judul": "Anti fragile",
+        "Penyanyi": "Le Sserafim",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+        "Duration": "232",
+        "Audio_path": "https://www.youtube.com/watch?v=pyf8cbqyfPs",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "album_id": "1",
+    },
+    {
+        "Judul": "Anti fragile",
+        "Penyanyi": "Le Sserafim",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+        "Duration": "232",
+        "Audio_path": "https://www.youtube.com/watch?v=pyf8cbqyfPs",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "album_id": "1",
+    },
+    {
+        "Judul": "Anti fragile",
+        "Penyanyi": "Le Sserafim",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+        "Duration": "232",
+        "Audio_path": "https://www.youtube.com/watch?v=pyf8cbqyfPs",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "album_id": "1",
+    },
+    {
+        "Judul": "Anti fragile",
+        "Penyanyi": "Le Sserafim",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+        "Duration": "232",
+        "Audio_path": "https://www.youtube.com/watch?v=pyf8cbqyfPs",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "album_id": "1",
+    },
+    {
+        "Judul": "Anti fragile",
+        "Penyanyi": "Le Sserafim",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+        "Duration": "232",
+        "Audio_path": "https://www.youtube.com/watch?v=pyf8cbqyfPs",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "album_id": "1",
+    },
+    {
+        "Judul": "Anti fragile",
+        "Penyanyi": "Le Sserafim",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+        "Duration": "232",
+        "Audio_path": "https://www.youtube.com/watch?v=pyf8cbqyfPs",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "album_id": "1",
+    },
+]
+
+albumList = [
+    {
+        "album_id": "1",
+        "Judul": "Antifragile",
+        "Penyanyi": "Le Sserafim",
+        "Total_duration": "654",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+    },
+    {
+        "album_id": "2",
+        "Judul": "Antifragile",
+        "Penyanyi": "Le Sserafim",
+        "Total_duration": "654",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+    },
+    {
+        "album_id": "3",
+        "Judul": "Antifragile",
+        "Penyanyi": "Le Sserafim",
+        "Total_duration": "654",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+    },
+    {
+        "album_id": "4",
+        "Judul": "Antifragile",
+        "Penyanyi": "Le Sserafim",
+        "Total_duration": "654",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+    },
+    {
+        "album_id": "5",
+        "Judul": "Antifragile",
+        "Penyanyi": "Le Sserafim",
+        "Total_duration": "654",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+    },
+    {
+        "album_id": "6",
+        "Judul": "Antifragile",
+        "Penyanyi": "Le Sserafim",
+        "Total_duration": "654",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+    },
+    {
+        "album_id": "7",
+        "Judul": "Antifragile",
+        "Penyanyi": "Le Sserafim",
+        "Total_duration": "654",
+        "Image_path": "../../assets/sample-song-image.jpg",
+        "Tanggal_terbit": "10/17/2022",
+        "Genre": "Pop",
+    },
+]
+
+filterPop = document.getElementById("filterPop");
+filterRock = document.getElementById("filterRock");
+filterJazz = document.getElementById("filterJazz");
+filterRnB = document.getElementById("filterRnB");
+sortAtoZ = document.getElementById("sortAtoZ");
+sortZtoA = document.getElementById("sortZtoA");
+
+function appendData(data, target) {
+    var mainContainer = document.getElementById(target);
+    let div = document.createElement("div");
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].penyanyi == null) {
+            data[i].penyanyi = "-";
+        }
+        if(data[i].genre == null){
+            data[i].genre = "-";
+        }
+        if(data[i].image_path == null){
+            data[i].image_path = "../../assets/basicimage.jpg";
+        }
+        div.innerHTML += '<div class="card"> \
+                    <img src="' + data[i].image_path + '" class="cardImage"> \
+                    <div class="songTitle">' + data[i].judul + '</div> \
+                    <div class="singer">' + data[i].penyanyi + '</div> \
+                    <div class="dateGenre"> \
+                        <div class="date">' + data[i].Tanggal_terbit + '</div> \
+                        <div class="genre">' + data[i].genre + '</div> \
+                    </div> \
+                </div>';
+    }
+    div.style.display = "flex";
+    div.style.flexDirection = "row";
+    div.style.flexWrap = "wrap";
+    div.style.justifyContent = "center";
+    div.style.alignItems = "center";
+    div.style.color = "white";
+    mainContainer.appendChild(div);
+    return div;
+}
+
+filterPop.addEventListener("click", function () {
+    // Get the data (Album & Song)
+    appendData(dataAlbum, "queryResultAlbum");
+    appendData(dataSong, "queryResultSong");
+});
+filterRock.addEventListener("click", function () {
+    // Get the data (Album & Song)
+    appendData(dataAlbum, "queryResultAlbum");
+    appendData(dataSong, "queryResultSong");
+});
+filterJazz.addEventListener("click", function () {
+    // Get the data (Album & Song)
+    appendData(dataAlbum, "queryResultAlbum");
+    appendData(dataSong, "queryResultSong");
+});
+filterRnB.addEventListener("click", function () {
+    // Get the data (Album & Song)
+    appendData(dataAlbum, "queryResultAlbum");
+    appendData(dataSong, "queryResultSong");
+});
+sortAtoZ.addEventListener("click", function () {
+    // Get the data (Album & Song)
+    appendData(dataAlbum, "queryResultAlbum");
+    appendData(dataSong, "queryResultSong");
+});
+sortZtoA.addEventListener("click", function () {
+    // Get the data (Album & Song)
+    appendData(dataAlbum, "queryResultAlbum");
+    appendData(dataSong, "queryResultSong");
+});
+
+window.onload = function() {
+    appendData(albumList, "queryResultAlbum");
+    appendData(songList, "queryResultSong");
+}
+
+// function searchFilter() {
+//     event.preventDefault();
+//     var input = document.getElementById("searchInput");
+//     var filter = input.value.toUpperCase();
+//     console.log(filter);
+//     var resultContainer = document.getElementById("queryResult");
+//     resultContainer.innerHTML = "";
+//     for (i = 0; i < songList.length; i++) {
+//         if (songList[i].Judul.toUpperCase().indexOf(filter) > -1) {
+//             resultContainer.innerHTML += `
+//             <div class="song">
+//                 <div class="song-image">
+//                     <img src="${songList[i].Image_path}" alt="">
+//                 </div>
+//                 <div class="song-info">
+//                     <div class="song-title">
+//                         <h3>${songList[i].Judul}</h3>
+//                     </div>
+//                     <div class="song-artist">
+//                         <h4>${songList[i].Penyanyi}</h4>
+//                     </div>
+//                     <div class="song-duration">
+//                         <h4>${songList[i].Tanggal_terbit}</h4>
+//                     </div>
+//                     <div class="song-genre">
+//                         <h4>${songList[i].Genre}</h4>
+//                     </div>
+//                 </div>
+//             </div>`
+//             resultContainer.style.display = "flex";
+//             resultContainer.style.flexDirection = "row";
+//             resultContainer.style.flexWrap = "wrap";
+//             resultContainer.style.justifyContent = "center";
+//             resultContainer.style.alignItems = "center";
+//         }
+//         if (songList[i].Penyanyi.toUpperCase().indexOf(filter) > -1) {
+//             resultContainer.innerHTML += `
+//             <div class="song">
+//                 <div class="song-image">
+//                     <img src="${songList[i].Image_path}" alt="">
+//                 </div>
+//                 <div class="song-info">
+//                     <div class="song-title">
+//                         <h3>${songList[i].Judul}</h3>
+//                     </div>
+//                     <div class="song-artist">
+//                         <h4>${songList[i].Penyanyi}</h4>
+//                     </div>
+//                     <div class="song-duration">
+//                         <h4>${songList[i].Tanggal_terbit}</h4>
+//                     </div>
+//                     <div class="song-genre">
+//                         <h4>${songList[i].Genre}</h4>
+//                     </div>
+//                 </div>
+//             </div>`
+//             resultContainer.style.display = "flex";
+//             resultContainer.style.flexDirection = "row";
+//             resultContainer.style.flexWrap = "wrap";
+//             resultContainer.style.justifyContent = "center";
+//             resultContainer.style.alignItems = "center";
+//         }
+//     }
+//     for (i = 0; i < albumList.length; i++) {
+//         if (albumList[i].Judul.toUpperCase().indexOf(filter) > -1) {
+//             resultContainer.innerHTML += `
+//             <div class="song">
+//                 <div class="song-image">
+//                     <img src="${albumList[i].Image_path}" alt="">
+//                 </div>
+//                 <div class="song-info">
+//                     <div class="song-title">
+//                         <h3>${albumList[i].Judul}</h3>
+//                     </div>
+//                     <div class="song-artist">
+//                         <h4>${albumList[i].Penyanyi}</h4>
+//                     </div>
+//                     <div class="song-duration">
+//                         <h4>${albumList[i].Total_duration}</h4>
+//                     </div>
+//                 </div>
+//             </div>`
+//             resultContainer.style.display = "flex";
+//             resultContainer.style.flexDirection = "row";
+//             resultContainer.style.flexWrap = "wrap";
+//             resultContainer.style.justifyContent = "center";
+//             resultContainer.style.alignItems = "center";
+//         }
+//     }
+// }
