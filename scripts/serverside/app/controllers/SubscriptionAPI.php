6e1ed46bb9b9011e7b4394b527bacf5d461f22ba
Bryan Bernigen <bryanbernigen@gmail.com>
2022-11-27 17:49:45 +0700

    models and api to handle subscriptions
    Benerin updatesubscription

---
+++ scripts/serverside/app/controllers/SubscriptionAPI.php
@@ -0,0 +1,57 @@
+<?php
+
+class SubscriptionAPI extends Controller {
+
+    public function newSubscription(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if(!isset($_POST['creator_id'])){
+                json_response_fail(WRONG_API_CALL);
+                return;
+            }
+            if(!isset($_POST['subscriber_id'])){
+                json_response_fail(WRONG_API_CALL);
+                return;
+            }
+            $res = $this->model('Subscription')->newSubscription($_POST['creator_id'], $_POST['subscriber_id']);
+            if($res){
+                json_response_success('New subscription created');
+            }
+            else{
+                json_response_fail('Failed to create new subscription');
+            }
+        }
+
+        else{
+            json_response_fail(METHOD_NOT_ALLOWED);
+            return;
+        }
+    }
+
+    public function updateSubscription(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (!isset($_POST["creator_id"])){
+                json_response_fail('WRONG_API_CALL');
+                return;
+            }
+            if (!(isset($_POST['subscriber_id']))) {
+                json_response_fail(WRONG_API_CALL);
+                return;
+            }
+            if (!(isset($_POST['status']))) {
+                json_response_fail(WRONG_API_CALL);
+                return;
+            }
+            $res = $this->model('Subscription')->updateSubscription($_POST['creator_id'], $_POST["subscriber_id"], $_POST['status']);
+            if ($res) {
+                json_response_success("success");
+            }else{
+                json_response_fail("failed");
+            }
+            return;
+        }
+        else{
+            json_response_fail(METHOD_NOT_ALLOWED);
+            return;
+        }
+    }
+}
