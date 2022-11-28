1754af6abdf09718ba71c79fde136e9f048eb8ba
Bryan Bernigen <bryanbernigen@gmail.com>
2022-11-28 16:17:37 +0700

    Integrate for SOAP service

--- scripts/serverside/app/core/App.php
+++ scripts/serverside/app/core/App.php
@@ -4,7 +4,11 @@
 class App {
     protected $params = [];
     public function __construct() {
-        header('Access-Control-Allow-Origin: http://localhost:8080');
+        if(isset($_SERVER['HTTP_ORIGIN'])){
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        }else{
+            header('Access-Control-Allow-Origin: *');
+        }
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
         header('Access-Control-Allow-Credentials: true');
